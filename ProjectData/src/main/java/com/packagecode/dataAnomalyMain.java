c8a7f45071543a1e97dd1c822d62f59c00bc9aff
Mayank Gupta <35414865+seekmayank@users.noreply.github.com>
2018-07-30 20:56:40 -0700

    Commit ver2.0.0

--- ProjectData/src/main/java/com/packagecode/dataAnomalyMain.java
+++ ProjectData/src/main/java/com/packagecode/dataAnomalyMain.java
@@ -1,58 +1,53 @@
 package com.packagecode;
 
-import java.util.Arrays;
-
 public class dataAnomalyMain {
 
-	public static void main(String[] args) {
-		
-		// TODO Auto-generated method stub
-		dataAnomalyAlgo obj = new dataAnomalyAlgo();	
-		
-		//Setter
-		obj.setSampleSize(60);
-
-		//Reading CSV data into an array
-		obj.getDataFileIntoArray(args[0]);
+    public static void main(String[] args) {
         
-		//Calculating Sample Mean & Standard Deviation for breaking data in multiple sample.
-		double[][] arryMeanStd = obj.calSampleMeanStandardDeviation();
-    	double meanArray[]=arryMeanStd[0];
-    	double stddevArray[]=arryMeanStd[1];
-
-		//Calculating Ceiling value of Z-Score
-        obj.calZScore(meanArray,stddevArray);
-
-		//Wring algorithm to see how many data point in sample are greater then 2 z-score 95% data
-		int[] arryAnomaly = obj.algoAnomalyDetection();
-
-		obj.getdataPoint(0,"Data");
-
-        System.out.println("\n");            
-        System.out.printf("Mean    arr[] : %s",Arrays.toString(meanArray));
-        System.out.println("\n");            
-        System.out.printf("Stddev  arr[] : %s",Arrays.toString(stddevArray));
-		obj.getdataPoint(2,"Z-Score");
-		obj.getdataPoint(3,"Anomaly");
-        System.out.println("\n");            
-        System.out.printf("Anomaly/Sample arr[] : %s",Arrays.toString(arryAnomaly));
+        // TODO Auto-generated method stub
+        dataAnomalyAlgo obj = new dataAnomalyAlgo();    
         
-        //If more then 5 data point then anomaly detected
-        //Ideal scenario: Only 3 data point should have more then 2 z-score in sample of 60.
-        //If more then 5 data point as our threshold. Then 6/60 which is 10%.
+        //Setting Sample Size
+        obj.setSampleSize(60);
+
+        //Reading CSV data into a data array
+        obj.getDataFileIntoArray(args[0]);
         
-        int max = Arrays.stream(arryAnomaly).max().orElse(0);
-        if(max>5) {
-        	System.out.println("\n");            
-        	System.out.printf("Failure :: Anomaly detected in data");
-        	System.out.println("\n");            
-            obj.showAnomalyData(max,arryAnomaly,meanArray,stddevArray);
-         
+        //Calculating Sample Mean & Standard Deviation for multiple sample
+        obj.calSampleMeanStandardDeviation();
+
+        //Calculating absolute value of Z-Score for all data point
+        obj.calZScore();
+
+        //Calculating one sample Z-Test
+        double maxZTestVal=obj.calZTest();
+
+        //Wring algorithm to see how many data point are either > 1.96 z-score or 60 z-test value
+        double maxAnomalyCnt=obj.algoAnomalyDetection();
+
+        obj.getdataPoint(0,"Data   ");
+        obj.getdataScore(0,"Mean   ");
+        obj.getdataScore(1,"Stddev ");
+        obj.getdataScore(2,"Z-Test ");
+        obj.getdataScore(3,"Anomaly");
+
+        if(maxAnomalyCnt >= 6 || maxZTestVal >= 60) {
+            System.out.println("\n");            
+            System.out.printf("Failure :: Anomaly detected in data");
+            System.out.println("\n");            
+            System.out.println("Anomaly Count : "+maxAnomalyCnt);
+            System.out.println("Z-Test Value  : "+maxZTestVal);
+            System.out.println("\n");            
+            obj.showAnomalyData(maxAnomalyCnt,maxZTestVal);
+            
         } else {
-        	System.out.println("\n");            
-        	System.out.printf("Success :: No anomaly found in data");
-        	System.out.println("\n");            
+            System.out.println("\n");            
+            System.out.printf("Success :: No anomaly found in data");
+            System.out.println("\n");           
+            System.out.println("Anomaly Count : "+maxAnomalyCnt);
+            System.out.println("Z-Test Value  : "+maxZTestVal);
+            System.out.println("\n");           
         }
         
-	}
+    }
 }
